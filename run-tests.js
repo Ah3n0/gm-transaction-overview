05c10552e2db351889a7b36d8081e70cb11a5f59
Ronny Ruhe <ronnyruhe@icloud.com>
2025-01-15 13:45:33 +0100

    updated run script

--- run-tests.js
+++ run-tests.js
@@ -1,19 +1,25 @@
-const { readdirSync } = require('fs');
-const { exec } = require('child_process');
-const path = require('path');
+const { readdirSync } = require("fs");
+const { exec } = require("child_process");
+const path = require("path");
 
-const testDir = path.resolve(__dirname, 'tests');
+const testDir = path.resolve(__dirname, "tests");
+const testFiles = readdirSync(testDir).filter((file) => file.endsWith(".test.js"));
 
-const testFiles = readdirSync(testDir).filter((file) => file.endsWith('.test.js'));
+let hasError = false;
 
 testFiles.forEach((file) => {
     const filePath = path.resolve(testDir, file);
     console.log(`Running test: ${file}`);
     exec(`node "${filePath}"`, (err, stdout, stderr) => {
         if (err) {
+            hasError = true;
             console.error(`Error running test: ${file}\n${stderr}`);
         } else {
             console.log(stdout);
         }
     });
+});
+
+process.on("exit", () => {
+    if (hasError) process.exit(1);
 });
