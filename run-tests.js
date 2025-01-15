e906b4473bc24c7a2aadb2a0fcd197ff25f2909c
Ronny Ruhe <ronnyruhe@icloud.com>
2025-01-15 14:06:28 +0100

    fixed worklow issue

--- run-tests.js
+++ run-tests.js
@@ -1,25 +1,19 @@
-const { readdirSync } = require("fs");
-const { exec } = require("child_process");
-const path = require("path");
+import { readdirSync } from "fs";
+import { exec } from "child_process";
+import path from "path";
 
-const testDir = path.resolve(__dirname, "tests");
+const testDir = path.resolve(path.dirname(import.meta.url), "../tests");
+
 const testFiles = readdirSync(testDir).filter((file) => file.endsWith(".test.js"));
-
-let hasError = false;
 
 testFiles.forEach((file) => {
     const filePath = path.resolve(testDir, file);
     console.log(`Running test: ${file}`);
     exec(`node "${filePath}"`, (err, stdout, stderr) => {
         if (err) {
-            hasError = true;
             console.error(`Error running test: ${file}\n${stderr}`);
         } else {
             console.log(stdout);
         }
     });
-});
-
-process.on("exit", () => {
-    if (hasError) process.exit(1);
 });
